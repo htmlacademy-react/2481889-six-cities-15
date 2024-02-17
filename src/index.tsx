3b6460cdc5a08c41a17382c9782269054c054362
Camille <KCHauenov@IBS.RU>
2024-02-18 00:13:05 +0400

    Added pages, created react components

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from '../src/components/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const rentAmount = 5;
 
 root.render(
   <React.StrictMode>
-    <h1>Hello, World!</h1>
+    <App rentAmount={rentAmount}/>
   </React.StrictMode>
 );
