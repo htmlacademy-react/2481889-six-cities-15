8fb151ea3c5814b146d5fe4a583da58d2d5d3e61
Camille <KCHauenov@IBS.RU>
2024-03-07 01:39:53 +0400

    Changed MainPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,12 +1,13 @@
 
 import { OfferList } from '../../components/OfferList/OfferList';
 import { Offer } from '../../types/offer';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import Map from '../../components/Map/Map';
 import { Amsterdam } from '../../mocks/city';
 import { Layout } from '../../components/Layout/Layout';
 import { NavLink } from 'react-router-dom';
 import { SortForm } from '../../components/SortForm/SortForm';
+import { Nullable } from '../../types/nullable';
 
 export type MainPageProps = {offers:Offer[]}
 
@@ -17,34 +18,36 @@ const cities = ['Paris', 'Collogne', 'Brussels','Hamburg', 'Amsterdam', 'Dusseld
     </NavLink>
   </li>));
 
-export const MainPage : FC<MainPageProps> = (props : MainPageProps) =>(
-  <Layout>
-    <div className="page page--gray page--main">
-
-      <main className="page__main page__main--index">
-        <h1 className="visually-hidden">Cities</h1>
-        <div className="tabs">
-          <section className="locations container">
-            <ul className="locations__list tabs__list">
-              {cities}
-            </ul>
-          </section>
-        </div>
-        <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">312 places to stay in Amsterdam</b>
-              <SortForm/>
-              <OfferList type="cities" offers={props.offers}/>
+export const MainPage : FC<MainPageProps> = (props : MainPageProps) => {
+  const [activeCard, setActiveCard] = useState<Nullable<Offer>>(null);
+  return (
+    <Layout>
+      <div className="page page--gray page--main">
+        <main className="page__main page__main--index">
+          <h1 className="visually-hidden">Cities</h1>
+          <div className="tabs">
+            <section className="locations container">
+              <ul className="locations__list tabs__list">
+                {cities}
+              </ul>
             </section>
-            <div className="cities__right-section">
-              <Map className='cities__map' city={Amsterdam} offers={props.offers} selectedOffer={null}/>
+          </div>
+          <div className="cities">
+            <div className="cities__places-container container">
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found">312 places to stay in Amsterdam</b>
+                <SortForm/>
+                <OfferList type="cities" offers={props.offers} setActiveCard={setActiveCard}/>
+              </section>
+              <div className="cities__right-section">
+                <Map className='cities__map' city={Amsterdam} offers={props.offers} selectedOffer={activeCard}/>
+              </div>
             </div>
           </div>
-        </div>
-      </main>
-    </div>
-  </Layout>
-);
+        </main>
+      </div>
+    </Layout>
+  );
+};
 export default MainPage;
