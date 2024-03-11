ba5fd69f538118a16084ea0811425d950de599d2
Camille <KCHauenov@IBS.RU>
2024-03-12 03:35:34 +0400

    Fixed issues

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App offers={offers} />
+      <App />
     </Provider>
   </React.StrictMode>
 );
