5df7dcaaae4beedd12cc5db88622df3017646d69
Camille <KCHauenov@IBS.RU>
2024-03-26 16:03:58 +0400

    Добавлена логика перенапавления

--- src/components/app-component/App.tsx
+++ src/components/app-component/App.tsx
@@ -6,23 +6,36 @@ import Error404 from '../../pages/error-page/error-page';
 import { Route, Routes} from 'react-router-dom';
 import { AppRoutes} from '../../constants';
 import PrivateRoute from '../private-route/private-route';
-import { useAppSelector } from '../../hooks/use-app';
-import { authSelectors } from '../../slices/auth';
 import HistoryRouter from '../history-route/history-route';
 import browserHistory from '../../browser-history';
+import { getToken } from '../../services/token';
+import { checkAuthAction } from '../../store/api-actions';
+import { useEffect } from 'react';
 
 
 function App(): JSX.Element{
-  const authorizationStatus = useAppSelector(authSelectors.authorizationStatus);
+  const token = getToken();
+  useEffect(() => {
+    if (token){
+      checkAuthAction();
+    }
+  }, [token]);
+
   return (
     <HistoryRouter history={browserHistory}>
       <Routes>
         <Route path={AppRoutes.Main}>
           <Route index element={<MainPage />} />
-          <Route path={AppRoutes.Login} element ={<LoginPage/>}/>
+          <Route path={AppRoutes.Login} element =
+            {
+              <PrivateRoute onlyUnAuth>
+                <LoginPage/>
+              </PrivateRoute>
+            }
+          />
           <Route path={AppRoutes.Favorites} element =
             {
-              <PrivateRoute authorizationStatus={authorizationStatus}>
+              <PrivateRoute>
                 <FavoritesPage />
               </PrivateRoute>
             }
