40ad647901dc57cc451aacdd1eca106c95a3cc0b
Camille <KCHauenov@IBS.RU>
2024-03-27 21:11:52 +0400

    Fixed euros

--- src/components/offer-card/offer-card.tsx
+++ src/components/offer-card/offer-card.tsx
@@ -41,7 +41,7 @@ export const OfferCard : FC<OfferCardsOffer> = ({offer, setActiveCard, className
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">{offer.price}</b>
+            <b className="place-card__price-value">€{offer.price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
           <button className="place-card__bookmark-button button" type="button">
