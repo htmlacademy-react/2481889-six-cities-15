15f39d61b05d5c1344e04f143672ed6e8950a7e3
Camille <KCHauenov@IBS.RU>
2024-04-03 00:13:23 +0400

    Fixed things

--- src/store/middlewares/redirect.ts
+++ src/store/middlewares/redirect.ts
@@ -1,16 +1,18 @@
-import {PayloadAction} from '@reduxjs/toolkit';
+import { Middleware, PayloadAction } from '@reduxjs/toolkit';
 import browserHistory from '../../browser-history';
-import {Middleware} from 'redux';
-import {rootReducer} from '../root-reducer';
+import { rootReducer } from '../root-reducer';
 
 type Reducer = ReturnType<typeof rootReducer>;
 
 export const redirect: Middleware<unknown, Reducer> =
   () =>
     (next) =>
-      (action: PayloadAction<string>) => {
-        if (action.type === 'game/redirectToRoute') {
-          browserHistory.push(action.payload);
+      (action: unknown) => {
+        if (typeof action === 'object' && action !== null && 'type' in action) {
+          const typedAction = action as PayloadAction<string>; // Приведение типа к PayloadAction<string>
+          if (typedAction.type === 'redirectToRoute') {
+            browserHistory.push(typedAction.payload);
+          }
         }
 
         return next(action);
