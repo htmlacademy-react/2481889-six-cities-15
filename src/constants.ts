1967402c679d4f6d0d9a9a969ad09bb1da2c7b5a
Camille <KCHauenov@IBS.RU>
2024-04-05 17:59:39 +0400

    Исправлены ошибки.

--- src/constants.ts
+++ src/constants.ts
@@ -16,9 +16,6 @@ export enum AuthorizationStatus {
     Unknown = 'UNKNOWN',
     Authorized = 'Authorized',
   }
-export const EnumerateConstants = {
-  rentAmount:5
-} as const;
 
 export const URL_MARKER_DEFAULT =
   'img/pin.svg';
